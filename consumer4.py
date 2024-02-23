e58e355d606a3c8f0a8f794fc9c5969a04ab43bc
niloofar <mousavi.n0@gmail.com>
2024-02-23 13:08:26 +0100

    offset comitting for inventory updating consumer

--- consumer4.py
+++ consumer4.py
@@ -1,9 +1,9 @@
 from kafka import KafkaConsumer
 import json
-from constants import PRODUCTS_DB_PATH, MIN_STOCK_LEVEL
+from constants import  MIN_STOCK_LEVEL,AUTO_COMMIT_OFFSET_MS
 from db_setup import make_inventory_if_not_exists
 
-
+import time
 
 
 
@@ -38,21 +38,25 @@ if __name__ == "__main__":
     consumer = KafkaConsumer(
         'Orders',
         bootstrap_servers='localhost:9092',
-        auto_offset_reset='latest', #only handle unhandled orders
+        auto_offset_reset='earliest', 
+        group_id= 'inventory_update', #if groupid is none, offset will not get updated. meaning we go through same orders again for a second run of the code
         enable_auto_commit=True,
         value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-        auto_commit_interval_ms=3000
+        auto_commit_interval_ms = AUTO_COMMIT_OFFSET_MS
     )
     try:
         for message in consumer:
-            #print(message)
+            # print(message)
             # print(json.dumps(message.value, indent=4))
+
+
             for order in message.value['order_details']:
                 product_id = order['product_id']
                 quantity = order['quantity']
                 update_product_balance(product_id,quantity,cursor,db)
     except KeyboardInterrupt:
         print('Closing')
+        time.sleep(AUTO_COMMIT_OFFSET_MS/1000 + 0.5) # to make sure offset is commited and we will not handle an order for the second time in the next run
     finally:
         consumer.close()
         cursor.close()
